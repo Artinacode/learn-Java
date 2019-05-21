30ece8aa82aa29d7d5910c2ba8a776dc2c61b811
80781650 <80781650@qq.com>
2019-05-21 23:45:49 +0800

    增加了leapyeardemo类

---
+++ src/Day002/leapYearDemo.java
@@ -0,0 +1,16 @@
+package Day002;
+
+import java.util.Scanner;
+
+public class leapYearDemo {
+    public static void main(String[] args) {
+        System.out.println("请输入年份：");
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        if(((year%4==0)&(year%100!=0))|(year%400==0))
+            System.out.println(year + "是闰年");
+        else
+            System.out.println(year + "不是闰年");
+
+    }
+}
