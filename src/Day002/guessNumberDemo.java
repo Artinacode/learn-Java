a54e25e065dcae5b7144e1163b1cc2dc43bd810a
80781650 <80781650@qq.com>
2019-05-23 12:55:46 +0800

    增加了游戏的一些细节 - 增加了次数的限制 - 不同次数猜对的评语 - 提示总共猜的次数

--- src/Day002/guessNumberDemo.java
+++ src/Day002/guessNumberDemo.java
@@ -1,21 +1,45 @@
 package Day002;
 
 import java.util.Scanner;
+
 //一个猜数字游戏
 public class guessNumberDemo {
     public static void main(String[] args) {
         int num = (int)(Math.random()*10);
        // System.out.println(num);
         int guess;
+        int k = 0;
         do{
             Scanner sc = new Scanner(System.in);
+            System.out.println("请在0~~~10之间猜一个数:");
             guess = sc.nextInt();
+            k++;
             if(guess<num)
                 System.out.println("It will be more");
             else System.out.println("It will be less");
         }
-        while(guess != num);
+        while(guess != num&&k<=5);
+        switch (k){
+            case 1:
+                System.out.println("聪明绝顶!");
+                System.out.println("您猜中了,答案是" + num );
+                System.out.println("一共猜了"+k+"次");
+                break;
+            case 2:
+                System.out.println("聪慧过人");System.out.println("您猜中了,答案是" + num );
+                System.out.println("一共猜了"+k+"次");break;
+            case 3:
+                System.out.println("You are so cute");System.out.println("您猜中了,答案是" + num );
+                System.out.println("一共猜了"+k+"次");break;
+            case 4:
+                System.out.println("oh 你终于猜对了");System.out.println("您猜中了,答案是" + num );
+                System.out.println("一共猜了"+k+"次");break;
+            case 5:
+                System.out.println("您是蒙对的吧");System.out.println("您猜中了,答案是" + num );
+                System.out.println("一共猜了"+k+"次");break;
+            default:
+                System.out.println("抱歉,5次机会已经用完了");break;
+        }
 
-        System.out.println("您猜中了,答案是" + num );
     }
 }
