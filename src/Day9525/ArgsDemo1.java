c203b19485bf950cb7adf033be63ea1f8a690b5d
80781650 <80781650@qq.com>
2019-05-26 00:06:23 +0800

    关于可变参数列表和重载的问题

--- src/Day9525/ArgsDemo1.java
+++ src/Day9525/ArgsDemo1.java
@@ -1,5 +1,10 @@
 package Day9525;
 //可变参数-查找
+
+//数组作为参数时,不能将多个值传递给数组
+//
+//一个方法中只能有一个可变参数列表
+//
 public class ArgsDemo1 {
     public void search(int n,int... a){
         boolean flag=false;
@@ -15,7 +20,9 @@ public class ArgsDemo1 {
             System.out.println("no");
         }
     }
+   // public  void search(int n,int[] a){
 
+   // }
     public static void main(String[] args) {
         ArgsDemo1 ad1=new ArgsDemo1();
         ad1.search(5,1,2,3,4,5);
---
+++ src/Day9525/ArgsDemo3.java
@@ -0,0 +1,29 @@
+package Day9525;
+/**
+ * 关于可变参数列表和重载的问题
+ * @author mengxing
+ * @version 1.0
+ *
+ */
+public class ArgsDemo3 {
+    //可变参数所在的方法最后被访问
+    //文档注释
+    public int plus(int a,int b){
+        System.out.println("不带可变参数的方法!");
+        return a+b;
+    }
+    public  int plus(int... a){
+        int sum=0;
+        for(int n:a){
+            sum+=n;
+        }
+        System.out.println("带可变参数的方法!");
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        ArgsDemo3 ad=new ArgsDemo3();
+        System.out.println(ad.plus(1,2));
+        System.out.println(ad.plus(1,2,3));
+    }
+}
